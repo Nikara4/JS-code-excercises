19074dcdbe816d3cc97c31b1f541a2cf989cb483
Aleksandra <olaszczur93@gmail.com>
2021-10-03 12:34:25 +0200

    solve problem with tasks number

--- to-do-app/js/toDoApp.js
+++ to-do-app/js/toDoApp.js
@@ -2,8 +2,10 @@
 class NewTask {
     constructor(task) {
         let _newTask = task;
-
         this.getTheTask = () => _newTask;
+
+        this.taskListItem = document.createElement('li');
+        this.taskListItem.innerHTML = this.getTheTask() + "<button>remove</button>";
     }
 
     checkForTask(value) {
@@ -16,45 +18,45 @@ class NewTask {
 
 class ToDoApp {
     constructor() {
-        this.toDoList = [];
 
+        this.toDoList = [];
         this.inputAdd = document.querySelector('[data-option="add-new"]');
         this.inputSearch = document.querySelector('[data-option="search-app"]');
         this.btnAdd = document.querySelector('section.add-task .add');
-
         this.tasksList = document.querySelector('div.task-list ul.tasks');
         this.tasksNumber = document.querySelector('div.task-list h3 span');
 
-
-        // taskListItem.dataset.key
         this.getInputValue = () => this.inputAdd.value;
-
-
-        // console.log(this.inputAdd);
-
-        // this.task = new NewTask(this.getAny());
-        // this.toDoList.push(this.task.getTheTask());
-        // console.log(this.toDoList)
-
-        // btnAdd.addEventListener('click', this.addNewTask.bind(this));
-        // // taskListItem.querySelector('button').addEventListener('click', removeDoneTask);
-
-        // if (value) {
-        //     this.toDoList.push(value);
         this.btnAdd.addEventListener('click', this.addNewElement.bind(this));
-        // this.listItemsArr = [...this.listItem];
-        // console.log(this.listItemsArr)
+        this.inputSearch.addEventListener('input', this.searchTasks.bind(this));
+    }
+    render() {
+        this.inputAdd.value = '';
 
-
+        this.toDoList.forEach((key, index) => {
+            this.task.taskListItem.dataset.key = index;
+        })
     }
     //usuwanie istniejącego zadania za pomocą buttona "Remove", który jest dodawany w klasie ToDoApp
     removeTask(e) {
         e.target.parentNode.remove();
-
-        const index = e.target.parentNode.dataset.key;
-        console.log(index);
-        this.toDoList.splice(index, 1);
+        this.index = e.target.parentNode.dataset.key;
+        this.toDoList.splice(this.index, 1);
         this.tasksNumber.textContent = this.toDoList.length;
+    }
+
+    //phase 3 - searching for tasks
+    searchTasks = (e) => {
+        this.searchText = e.target.value.toLowerCase();
+        console.log(this.searchText);
+        // let that = this;
+        const filterTasks = this.toDoList.filter(element => {
+            element.toLowerCase().includes(this.searchText);
+        });
+
+        this.tasksList.textContent = '';
+        filterTasks.forEach(task => this.tasksList.appendChild(task));
+        console.log(filterTasks);
     }
 
     //dodawanie nowego elementu DOM
@@ -62,80 +64,17 @@ class ToDoApp {
         this.task = new NewTask(this.getInputValue());
         if (this.task.checkForTask()) return alert('no task specified');
 
-        let taskListItem = document.createElement('li');
-        taskListItem.className = 'one-task';
-        this.tasksList.appendChild(taskListItem)
-        taskListItem.innerHTML = this.task.getTheTask() + "<button>remove</button>";
-
-        this.toDoList.forEach((key, index) => {
-            taskListItem.dataset.key = index;
-        })
-
+        this.tasksList.appendChild(this.task.taskListItem)
         this.toDoList.push(this.task.getTheTask());
         this.tasksNumber.textContent = this.toDoList.length;
 
-        taskListItem.querySelector('button').addEventListener('click', this.removeTask);
-    }
+        this.task.taskListItem.querySelector('button').addEventListener('click', this.removeTask.bind(this));
+        this.render();
 
+    }
 }
+
 const newApp = new ToDoApp();
 
-/*
 
-
-
-
-//phase 3 - searching for tasks
-class Search {
-    static searchTasks = (e) => {
-        this.searchText = e.target.value.toLowerCase();
-        this.newTasksArr = toDoList.filter(task => task.textContent.toLowerCase().includes(searchText));
-        // tasksList.textContent = '';
-        // newTasksArr.forEach(task => tasksList.appendChild(task));
-    }
-}
-
-//importing DOM objects from HTML
-
-class ToDoApp {
-    constructor() {
-        // this.toDoList = [];
-        this.inputAdd = document.querySelector('[data-option="add-new"]');
-        this.inputSearch = document.querySelector('[data-option="search-app"]');
-        this.btnAdd = document.querySelector('section.add-task .add');
-
-        this.tasksList = document.querySelector('div.task-list ul.tasks');
-        this.listItem = document.getElementsByClassName('one-task');
-        this.tasksNumber = document.querySelector('div.task-list h3 span');
-
-        this.task = new NewTask(inputAdd.value);
-        this.remove = new RemoveTask();
-
-        btnAdd.addEventListener('click', this.addNewTask.bind(this));
-        // taskListItem.querySelector('button').addEventListener('click', removeDoneTask);
-
-
-
-    }
-    //rendering list - adding data-key with number according to array index ('key is index')
-    render() {
-        tasksList.textContent = "";
-        this.remove.getTheList().forEach((element, key) => {
-            element.dataset.key = key;
-            tasksList.appendChild(element);
-        })
-    }
-
-    addNewTask() {
-        const taskList = document.createElement('ul');
-        taskList.className = 'task-list';
-        taskList.textContent = task.tasksList();
-        taskListItem.className = 'one-task';
-        taskListItem.innerHTML = task.getNewTask() + "<button>remove</button>";
-        task.tasksList().push(taskListItem);
-        // 
-        // if (this.task.checkForTask())
-
-    };
-}*/
 
